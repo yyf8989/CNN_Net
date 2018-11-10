d8130d53f8c7b89e0a959ebf6d1abb7127a61133
yyf8989 <295677672@qq.com>
2018-11-10 22:50:26 +0800

    画图函数添加

--- test01.py
+++ test01.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-x = np.array(range(12)).reshape((2,2,3))
+x = np.array(range(12)).reshape((2, 2, 3))
 print(x)
 # 打印array reshaoe之后的形状
 
@@ -9,3 +9,11 @@ y = np.reshape(x, (-1, 6))
 print(y)
 
 print()
+
+import matplotlib.pyplot as plt
+
+a = [1, 2, 3, 4]
+b = [2, 4, 6, 8]
+
+plt.plot(a, b)
+plt.show()
